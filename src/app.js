72f05d2c93d5de6b516acdc1fae145a6c85749ba
Shawon <hasebulhasan97@gmail.com>
2023-11-20 01:22:23 +0600

    {ENH]: add post method on app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const request = require('request');
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/test", (_req, res) => {
   res.status(200).send("Hello world");
@@ -14,5 +16,19 @@ app.get("/cse408", (_req, res) => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.post('/showpost', (req, res) => {
+
+  const url = `https://secure.shippingapis.com/ShippingAPI.dll?API=ZipCodeLookup&XML=<ZipCodeLookupRequest USERID="661PARAP3185">
+              <Address>
+              <Address1>${req.body.address1}</Address1>
+              <Address2>${req.body.address2}</Address2>
+              <City>${req.body.city}</City>
+              <State>${req.body.state}</State>
+              </Address>
+              </ZipCodeLookupRequest>`;
+  console.log(url)
+  res.status(200).send(url)
+})
 
 module.exports = app;
