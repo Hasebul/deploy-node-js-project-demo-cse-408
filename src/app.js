a77a712ba448517ee0faa2c8a8d64b1166f25d42
Shawon <hasebulhasan97@gmail.com>
2023-11-20 00:39:08 +0600

    [ENH] : Add new api

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ app.get("/test", (_req, res) => {
   res.status(200).send("Hello world");
 });
 
+app.get("/cse408", (_req, res) => {
+  res.status(200).send("Are you ready for Deploy your own project on aws ecr?");
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
